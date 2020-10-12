4af08f5451bc1892911b29ee909651ed5c181aaa
Cadertea <cadertea@ncsu.edu>
2020-10-12 12:09:42 -0400

    code is working need to add the clear function and perhaps some css for my buttons

--- js.js
+++ js.js
@@ -1,63 +1,137 @@
-$("#search-button").on("click", function() {
-    // In this case, the "this" keyword refers to the button that was clicked
-    var cityInput = $('#city-input').val();
+var historycity = JSON.parse(localStorage.getItem('history')) || []
 
-    // Constructing a URL to search Giphy for the name of the person who said the quote
-    var queryURL = " http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=c2346b5853a5f70fdfb971c5c6033ff2";
+$("#search-button").on("click", function () {
 
-    // Performing our AJAX GET request
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // After the data comes back from the API
-      .then(function(response) {
-          console.log(queryURL);
-          console.log(response);
-// storing properties into variables for easy access
-          var name = response.name;
-          var temp = response.main.temp
-          var humidity = response.main.humidity;
-          var wind = response.wind.
-          //adding properties into my html dynamically
-          $('#city-name').html('<h3>'+ name)
-          $('#temperature').html('<h6>'+ 'Temperature: ' + temp)
-          $('#humidity').html('<h6>'+ 'Humidity: ' + humidity+ '%')
-          $('#wind-speed').html('<h6>'+ 'Wind-Speed: ' + wind + ' MPH')
-        // Storing an array of results in the results variable
-        // var results = response.data;
+  var cityInput = $('#city-input').val();
+  callingforcast(cityInput);
+  query3(cityInput);
+  //issues putting data into array and giving it a unique index
 
-        // // Looping over every result item
-        // for (var i = 0; i < results.length; i++) {
 
-        //   // Only taking action if the photo has an appropriate rating
-        //   if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-        //     // Creating a div for the gif
-        //     var gifDiv = $("<div>");
+});
 
-        //     // Storing the result item's rating
-        //     var rating = results[i].rating;
+function callingforcast(cityInput) {
 
-        //     // Creating a paragraph tag with the result item's rating
-        //     var p = $("<p>").text("Rating: " + rating);
+  // Constructing a URL to search Giphy for the name of the person who said the quote
+  var queryURL = " http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=c2346b5853a5f70fdfb971c5c6033ff2";
 
-        //     // Creating an image tag
-        //     var personImage = $("<img>");
+  // Performing our AJAX GET request
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // After the data comes back from the API
+    .then(function (response) {
+      // console.log(queryURL);
+      console.log(response);
 
-        //     // Giving the image tag an src attribute of a proprty pulled off the
-        //     // result item
-        //     personImage.attr("src", results[i].images.fixed_height.url);
+      if (historycity.indexOf(cityInput) === -1) {
 
-        //     // Appending the paragraph and personImage we created to the "gifDiv" div we created
-        //     gifDiv.append(p);
-        //     gifDiv.append(personImage);
+        historycity.push(cityInput)
+        localStorage.setItem("history", JSON.stringify(historycity))
+        display()
+      }
+      // storing properties into variables for easy access
+      var name = response.name;
+      var temp = response.main.temp
+      var tempF = (((temp - 273.15) * 1.8) + 32).toFixed(2)
+      var humidity = response.main.humidity;
+      var windSpeed = response.wind.speed
 
-        //     // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-        //     $("#gifs-appear-here").prepend(gifDiv);
-          // }
-        // }
-      });
-  });
+      var lat = response.coord.lat
+      var lon = response.coord.lon
 
+      //adding properties into my html dynamically
+
+      $('#city-name').html('<h3>' + name + ' ' + moment().format('L'))
+      $('#temperature').html('<h6>' + 'Temperature: ' + tempF + '<b>℉</b>')
+      $('#humidity').html('<h6>' + 'Humidity: ' + humidity + '%')
+      $('#wind-speed').html('<h6>' + 'Wind-Speed: ' + windSpeed + ' MPH')
+      // $('#UV-index').html('<h6>'+ 'UV-Index: ' + windSpeed + ' MPH')
+
+      var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=c2346b5853a5f70fdfb971c5c6033ff2";
+      // console.log(queryURL2);
+      $.ajax({
+        url: queryURL2,
+        method: "GET"
+      })
+        // After the data comes back from the API
+        .then(function (response2) {
+          console.log(response2);
+          var uv = response2.value;
+          // sets the uv index is to a var color
+          var color = $("#UV-index").html('<h6>' + 'UV: ' + uv)
+
+          // changes color based on uv value
+          if (uv <= 3) {
+            color.css('background-color', 'red')
+            color.css('color', 'black')
+
+          } else if (uv > 3 && uv <= 7) {
+            color.css('background-color', 'yellow')
+            color.css('color', 'black')
+
+          } else {
+            color.css('background-color', 'purple')
+            color.css('color', 'white')
+          }
+        });
+    });
+
+};
+//end of funciton paren. 
+function query3(cityInput) {
+
+  var queryURL3 = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=c2346b5853a5f70fdfb971c5c6033ff2&units=imperial";
+  // console.log(queryURL3);
+  $.ajax({
+    url: queryURL3,
+    method: "GET"
+  })
+
+    .then(function (response3) {
+      console.log(response3);
+      var forcast = "";
+      for (let i = 0; i < response3.list.length; i = i + 8) {
+        forcast += `<div class="col forecast bg-primary text-white ml-3 mb-3 rounded">
+        ${response3.list[i].dt_txt}
+       <p> Temperature: ${response3.list[i].main.temp}</p>
+        <img src="http://openweathermap.org/img/wn/${response3.list[i].weather[0].icon}@2x.png" />
+        <p>wind speed :${response3.list[i].wind.speed}<p>
+        <p>Description: ${response3.list[i].weather[0].description}<p>
+        </div>
+        `
+
+
+      }
+
+      $('#forcast').html(forcast)
+
+
+
+    });
+
+}
+
+function display() {
+  var historycity = JSON.parse(localStorage.getItem('history')) || []
+  var html = ""
+  for (var q = 0; q < historycity.length; q++) {
+    html += `<button class='previuosCity'>${historycity[q]}</button>`
+
+
+  }
+  console.log(html)
+  $('#history').html(html)
+
+}
+$('#history').on('click', ".previuosCity", function () {
+  var cityInput = $(this).text()
+  console.log(cityInput);
+  callingforcast(cityInput);
+  query3(cityInput);
+
+});
+display()
   // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
+ // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
